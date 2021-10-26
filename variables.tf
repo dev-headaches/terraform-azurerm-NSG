fcda20cb89424e1d7e244c252fb21b9668d3fd64
nbeam85 <92865313+nbeam85@users.noreply.github.com>
2021-10-26 15:41:41 -0400

    removed prjname and other unneeded vars

--- variables.tf
+++ variables.tf
@@ -1,18 +1,3 @@
-variable "enviro" {
-  type        = string
-  description = "define the environment ex. dev,tst,prd,stg"
-}
-
-variable "prjname" {
-  type        = string
-  description = "define the project name ex. prj02"
-}
-
-variable "prjnum" {
-  type        = string
-  description = "define the project number for TFstate file ex. 4858"
-}
-
 variable "nsgname" {
   type        = string
   description = "the resource name of the NSG being created"
